9cad87951e07cd57399a1e07dad7a6809f924b0b
Jptigre <joaopeedrorodrigues@gmail.com>
2025-06-04 21:23:56 -0300

    aaaaa

--- script-04.js
+++ script-04.js
@@ -1,14 +1,24 @@
 function media(){
+  //+-------------------
+  //|Recebe as notas 
+  //+-------------------
   var a = parseFloat(document.getElementById('valor01').value);
   var b = parseFloat(document.getElementById('valor02').value);
-  var m = (a+b)/2;
+  var c = parseFloat(document.getElementById('valor03').value);
+  
+  //+-------------------
+  // |Cálculo de média
+  //+-------------------
+  
+  var maior = a;
+  
+  if (b > maior) {
+    maior = b; 
+  } 
 
-  if(m == 10){
-    document.getElementById('demo').innerHTML = "Média = "+ m.toString() + "(Aprovado com Distinção)";
-  } else if (m>=7) {
-    document.getElementById('demo').innerHTML = "Média = "+ m.toString() + "(Aprovado)";
-  }else{
-    document.getElementById('demo').innerHTML = "Média = "+ m.toString() + "(Reprovado)";
+  if(c > maior){
+    maior = c;
   }
-
+  
+  document.getElementById("demo").innerHTML = "Maior Valor = " + maior.toString();
 }
